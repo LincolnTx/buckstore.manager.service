226b2782c8fddc4d67d4299b86b1389bc0739961
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-06-18 11:06:24 -0300

    feat: preparing kafka producers and consumers
    Add kafka info on settings
    Create a bus project layer
    Create base classes for consumer and producers

---
+++ src/buckstore.manager.service.application/IntegrationEvents/IntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MediatR;
+
+namespace buckstore.manager.service.application.IntegrationEvents
+{
+    public class IntegrationEvent : INotification
+    {
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ src/buckstore.manager.service.environment/Configuration/KafkaConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace buckstore.manager.service.environment.Configuration
+{
+    public class KafkaConfiguration
+    {
+        public string ConnectionString { get; set; }
+        public string Group { get; set; }
+        public string ManagerToProductsCreate { get; set; }
+        public string ManagerToProductsUpdate { get; set; }
+        public string ManagerToProductsDelete { get; set; }
+        public string ManagerToOrdersCreate { get; set; }
+        public string ManagerToOrdersUpdate { get; set; }
+        public string ManagerToOrdersDelete { get; set; }
+    }
+}
