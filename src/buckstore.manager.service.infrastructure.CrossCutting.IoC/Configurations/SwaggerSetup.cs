6f866fd6317d62ad26f9a812e270fe2b55316005
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-06-18 10:33:57 -0300

    configure swagger

--- src/buckstore.manager.service.infrastructure.CrossCutting.IoC/Configurations/SwaggerSetup.cs
+++ src/buckstore.manager.service.infrastructure.CrossCutting.IoC/Configurations/SwaggerSetup.cs
@@ -1,7 +1,10 @@
 using System;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Builder;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace buckstore.manager.service.infrastructure.CrossCutting.IoC.Configurations
 {
@@ -10,16 +13,35 @@ namespace buckstore.manager.service.infrastructure.CrossCutting.IoC.Configuratio
 		public static void AddSwaggerSetup(this IServiceCollection services)
 		{
 			if (services == null) throw new ArgumentNullException(nameof(services));
-			services.AddSwaggerGen(s =>
+			services.AddSwaggerGen(c =>
 			{
-				s.SwaggerDoc("v1", new OpenApiInfo
+				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
 				{
-					Version = "v1",
-					Title = "BuckStore Manager Api",
-					Description = "Api responsável pelas assões de gerenciamento do E-Commerce",
-					Contact = new OpenApiContact { Name = "Lincoln Teixeira", Email = "lincolnsf98@gmail.com" }
+					Description = "Token JWT de autorização Scheme Bearer",
+					Name = "Authorization",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.Http,
+					Scheme = "bearer"
 				});
+				
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							}
+						}, new List<string>()
+					}
+				});
+				// c.OperationFilter<RemoveVersionParameterFilter>();
+				// c.DocumentFilter<ReplaceVersionWithExactValueInPathFilter>();
 			});
+			
+			services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 		}
 		
 		public static void UseSwaggerSetup(this IApplicationBuilder app)
@@ -29,6 +51,20 @@ namespace buckstore.manager.service.infrastructure.CrossCutting.IoC.Configuratio
 			app.UseSwaggerUI(c =>
 			{
 				c.SwaggerEndpoint("/swagger/v1/swagger.json", "BuckStore Manager Api");
+			});
+		}
+	}
+	
+	public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+	{
+		public void Configure(SwaggerGenOptions options)
+		{
+			options.SwaggerDoc("v1", new OpenApiInfo
+			{
+				Version = "v1",
+				Title = "BuckStore Manager Api",
+				Description = "Api responsável pelas assões de gerenciamento do E-Commerce",
+				Contact = new OpenApiContact { Name = "Lincoln Teixeira", Email = "lincolnsf98@gmail.com" }
 			});
 		}
 	}
