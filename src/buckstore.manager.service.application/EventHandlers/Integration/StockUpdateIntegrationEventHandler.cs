5a95e54e24fb7283735f912695280e9f67f24378
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-09-15 22:27:44 -0300

    feat: getting stock update message and change database

--- src/buckstore.manager.service.application/EventHandlers/Integration/StockUpdateIntegrationEventHandler.cs
+++ src/buckstore.manager.service.application/EventHandlers/Integration/StockUpdateIntegrationEventHandler.cs
@@ -1,14 +1,32 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using buckstore.manager.service.domain.SeedWork;
 using buckstore.manager.service.application.IntegrationEvents;
+using buckstore.manager.service.domain.Aggregates.ProductAggregate;
 
 namespace buckstore.manager.service.application.EventHandlers.Integration
 {
     public class StockUpdateIntegrationEventHandler : EventHandler<StockUpdateIntegrationEvent>
     {
-        public override Task Handle(StockUpdateIntegrationEvent notification, CancellationToken cancellationToken)
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _uow;
+
+        public StockUpdateIntegrationEventHandler(IProductRepository productRepository, IUnitOfWork uow)
         {
-            throw new System.NotImplementedException();
+            _productRepository = productRepository;
+            _uow = uow;
+        }
+
+        public override async Task Handle(StockUpdateIntegrationEvent notification, CancellationToken cancellationToken)
+        {
+            foreach (var product in notification.Products)
+            {
+                var response = await _productRepository.FindById(product.ProductId);
+
+                response.DeductStock(product.Quantity);
+            }
+
+            await _uow.Commit();
         }
     }
 }
--- src/buckstore.manager.service.application/IntegrationEvents/StockUpdateIntegrationEvent.cs
+++ src/buckstore.manager.service.application/IntegrationEvents/StockUpdateIntegrationEvent.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
+using buckstore.manager.service.application.Adapters.MessageBroker.Dtos;
 
 namespace buckstore.manager.service.application.IntegrationEvents
 {
     public class StockUpdateIntegrationEvent : IntegrationEvent
     {
         public Guid OrderId { get; set; }
-        public IEnumerable StockUpdateItemsDto { get; set; }
+        public IEnumerable<StockUpdateItemsDto> Products  { get; set; }
 
         public StockUpdateIntegrationEvent() : base(DateTime.Now)
         {
