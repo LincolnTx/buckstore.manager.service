0e12b76450cb1f6ccbfb86f9aedc09f67782e74a
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-11-28 11:16:32 -0300

    feat: create a delete sales flow
    command, handler, validation and all

---
+++ src/buckstore.manager.service.application/CommandHandlers/DeleteSaleCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using buckstore.manager.service.domain.SeedWork;
+using buckstore.manager.service.domain.Exceptions;
+using buckstore.manager.service.application.Commands;
+using buckstore.manager.service.domain.Aggregates.SalesAggregate;
+
+namespace buckstore.manager.service.application.CommandHandlers
+{
+    public class DeleteSaleCommandHandler : CommandHandler, IRequestHandler<DeleteSaleCommand, bool>
+    {
+        private readonly ISaleRepository _saleRepository;
+
+        public DeleteSaleCommandHandler(IUnitOfWork uow,
+            IMediator bus,
+            INotificationHandler<ExceptionNotification> notifications,
+            ISaleRepository saleRepository) : base(uow,
+            bus,
+            notifications)
+        {
+            _saleRepository = saleRepository;
+        }
+
+        public async Task<bool> Handle(DeleteSaleCommand request, CancellationToken cancellationToken)
+        {
+            if (!request.IsValid())
+            {
+                NotifyValidationErrors(request);
+                return false;
+            }
+
+            var sale = _saleRepository.FindById(request.Id);
+
+            if (sale == null)
+            {
+                await _bus.Publish(new ExceptionNotification("056", "Não foi encontrado nenhuma promoção com este Id"),
+                    cancellationToken);
+
+                return false;
+            }
+
+            _saleRepository.Delete(sale);
+
+            if (await Commit())
+                return true;
+
+            await _bus.Publish(new ExceptionNotification("056", "Não foi possível realizar a remoção desse cupom"),
+                cancellationToken);
+
+            return false;
+
+        }
+    }
+}
---
+++ src/buckstore.manager.service.application/Commands/DeleteSaleCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using buckstore.manager.service.application.Validations;
+using MediatR;
+
+namespace buckstore.manager.service.application.Commands
+{
+    public class DeleteSaleCommand : Command, IRequest<bool>
+    {
+        public Guid Id { get; set; }
+        public override bool IsValid()
+        {
+            ValidationResult = new DeleteSaleCommandValidation().Validate(this);
+
+            return ValidationResult.IsValid;
+        }
+    }
+}
