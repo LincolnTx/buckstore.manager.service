86846d399df5834d9def01820d3f892c7903fd5b
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-11-30 09:50:36 -0300

    adding url to start

--- src/buckstore.manager.service.api.v1/Program.cs
+++ src/buckstore.manager.service.api.v1/Program.cs
@@ -18,6 +18,10 @@ namespace buckstore.manager.service.api.v1
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:3003");
+                });
 	}
 }
